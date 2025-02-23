311f7d918b4a43cad388df71e6cf7a3b23a4a7c4
“Renato <casrenat@amazon.com>
2025-02-23 11:22:10 -0300

    Visual

--- src/model/main.py
+++ src/model/main.py
@@ -4,7 +4,7 @@ def main():
     """Função principal"""    
     analyzer = MediaBiasAnalyzer()
     
-    #analyzer.train_model(training_data="../../data/speech/Discursos_Enriquecidos.csv")
+    analyzer.train_model(training_data="../../data/speech/Discursos_Enriquecidos.csv")
     analyzer.analyze_media()
 
 if __name__ == "__main__":
